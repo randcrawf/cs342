de4d6df8af9e3ad32dc9de5a56757e58a394e2d9
Randall Crawford <rmc3637@utexas.edu>
2021-10-17 23:49:44 -0500

    added debugging print statements

--- homework3/homework/models.py
+++ homework3/homework/models.py
@@ -53,7 +53,7 @@ class CNNClassifier(torch.nn.Module):
         l = [torch.nn.Conv2d(3, c, kernel_size=7, padding=3, stride=2), torch.nn.BatchNorm2d(c), torch.nn.ReLU(), torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1)]
         layers = [32, 64, 128]
         for layer in layers:
-            l.append(self.Block(c, layer, stride=1))
+            l.append(self.Block(c, layer, stride=2))
             c = layer
 
         self.feature_extractor = torch.nn.Sequential(*l)
