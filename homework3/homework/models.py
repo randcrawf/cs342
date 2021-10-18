ff965ed2f958868d06d40d778cc73517f4cf69d7
Randall Crawford <rmc3637@utexas.edu>
2021-10-17 23:46:27 -0500

    added debugging print statements

--- homework3/homework/models.py
+++ homework3/homework/models.py
@@ -38,8 +38,8 @@ class CNNClassifier(torch.nn.Module):
 
         def forward(self, x):
             identity = x
-            if self.downsample is not None:
-                identity = self.downsample(x)
+            # if self.downsample is not None:
+            #     identity = self.downsample(x)
             return self.net(x) + identity
 
     def __init__(self):
