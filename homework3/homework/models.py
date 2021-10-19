da1ab498a6840a21bf21906f944f8eeb1182775a
Randall Crawford <rmc3637@utexas.edu>
2021-10-19 15:13:00 -0500

    debugging

--- homework3/homework/models.py
+++ homework3/homework/models.py
@@ -113,8 +113,8 @@ class FCN(torch.nn.Module):
           torch.nn.BatchNorm2d(32),
           torch.nn.ReLU(),
           torch.nn.UpsamplingBilinear2d(scale_factor=2),
-          torch.nn.Conv2d(32, 6, kernel_size=3, stride=1, padding=1),
-          torch.nn.BatchNorm2d(6),
+          torch.nn.Conv2d(32, 5, kernel_size=3, stride=1, padding=1),
+          torch.nn.BatchNorm2d(5),
           torch.nn.ReLU()
         )
 
