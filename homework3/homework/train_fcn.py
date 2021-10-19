0fb5d11262175fe8a0401176c934fcc2953405db
Randall Crawford <rmc3637@utexas.edu>
2021-10-19 14:52:16 -0500

    debugging

--- homework3/homework/train_fcn.py
+++ homework3/homework/train_fcn.py
@@ -32,7 +32,7 @@ def train(args):
 
     optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, momentum=args.momentum, weight_decay=args.weight_decay)
     loss = ClassificationLoss()
-    t = dense_transforms.Compose((dense_transforms.ColorJitter(0.3, 0.3, 0.3, 0.3), dense_transforms.RandomHorizontalFlip(), dense_transforms.RandomCrop(96), dense_transforms.ToTensor()))
+    t = dense_transforms.Compose((dense_transforms.ColorJitter(0.3, 0.3, 0.3, 0.3), dense_transforms.RandomHorizontalFlip(), dense_transforms.ToTensor()))
     print("Loading data...")
     train_data = load_dense_data('dense_data/train', transform=t)
     valid_data = load_dense_data('dense_data/valid')
