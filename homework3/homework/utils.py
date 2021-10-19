57af190a4df9027029b32ac2d6361887dd3eeb98
Randall Crawford <rmc3637@utexas.edu>
2021-10-19 00:56:39 -0500

    debugging

--- homework3/homework/utils.py
+++ homework3/homework/utils.py
@@ -43,7 +43,8 @@ class SuperTuxDataset(Dataset):
         if mode == 'train':
             self.t = transforms.Compose([
                         transforms.ToPILImage(),
-                        transforms.RandomCrop(32),
+                        transforms.Scale(256),
+                        transforms.RandomCrop(224),
                         transforms.RandomHorizontalFlip(),
                         transforms.ToTensor(),
                         transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
