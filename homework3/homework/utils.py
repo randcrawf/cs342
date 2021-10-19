e8e33704af323a3d2ced98fb0cca696609f289c5
Randall Crawford <rmc3637@utexas.edu>
2021-10-19 01:49:35 -0500

    debugging

--- homework3/homework/utils.py
+++ homework3/homework/utils.py
@@ -51,7 +51,7 @@ class SuperTuxDataset(Dataset):
             self.t = transforms.Compose([
                         transforms.ToPILImage(),
                         transforms.Scale(32),
-                        transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
+                        transforms.ToTensor()
                     ])
     def __len__(self):
         """
