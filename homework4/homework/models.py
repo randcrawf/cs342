e875af3ec27e345a16905b06993288c2aac02c1b
Randall Crawford <rmc3637@utexas.edu>
2021-10-25 20:38:55 -0500

    commit

--- homework4/homework/models.py
+++ homework4/homework/models.py
@@ -1,6 +1,21 @@
 import torch
 import torch.nn.functional as F
 
+class ClassificationLoss(torch.nn.Module):
+    def forward(self, input, target):
+        """
+        Your code here
+
+        Compute mean(-log(softmax(input)_label))
+
+        @input:  torch.Tensor((B,C))
+        @target: torch.Tensor((B,), dtype=torch.int64)
+
+        @return:  torch.Tensor((,))
+
+        Hint: Don't be too fancy, this is a one-liner
+        """
+        return F.cross_entropy(input, target)
 
 def extract_peak(heatmap, max_pool_ks=7, min_score=-5, max_det=100):
     """
@@ -12,17 +27,94 @@ def extract_peak(heatmap, max_pool_ks=7, min_score=-5, max_det=100):
        @return: List of peaks [(score, cx, cy), ...], where cx, cy are the position of a peak and score is the
                 heatmap value at the peak. Return no more than max_det peaks per image
     """
-    raise NotImplementedError('extract_peak')
+    max_cls, indices = F.max_pool2d(heatmap[None, None], kernel_size=max_pool_ks, padding=max_pool_ks // 2, stride=1, return_indices=True)
+
+    # tip: visualize is_peak and heatmap side by side.
+    is_peak = (heatmap >= max_cls).float()
+
+    mask = torch.logical_and(max_cls > min_score, is_peak == 1.0)
+    max_cls = max_cls[mask]
+    indices = indices[mask]
+    peaks, i = torch.topk(max_cls, min(max_det, len(max_cls)))
+
+    return [*zip(peaks, indices[i] % heatmap.shape[1], indices[i] // heatmap.shape[1])]
+
+class CNNClassifier(torch.nn.Module):
+    class Block(torch.nn.Module):
+        def __init__(self, n_input, n_output, stride=1):
+            super().__init__()
+            self.net = torch.nn.Sequential(
+                torch.nn.Conv2d(n_input, n_output, kernel_size=3, stride=stride, padding=1, bias=False),
+                torch.nn.BatchNorm2d(n_output),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(n_output, n_output, kernel_size=3, padding=1, bias=False),
+                torch.nn.BatchNorm2d(n_output),
+                torch.nn.ReLU()
+            )
+            
+        def forward(self, x):
+            return self.net(x)
+
+    def __init__(self):
+        super().__init__()
+        """
+        Your code here
+        Hint: Base this on yours or HW2 master solution if you'd like.
+        Hint: Overall model can be similar to HW2, but you likely need some architecture changes (e.g. ResNets)
+        """
+        c = 32
+        l = [torch.nn.Conv2d(3, c, kernel_size=7, padding=3, stride=2, bias=False), torch.nn.BatchNorm2d(c), torch.nn.ReLU(), torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1)]
+        layers = [32, 64, 128]
+        for layer in layers:
+            l.append(self.Block(c, layer, stride=2))
+            c = layer
+
+        self.feature_extractor = torch.nn.Sequential(*l)
+        self.classifier = torch.nn.Linear(c, 10)
+
+    def forward(self, x):
+        """
+        Your code here
+        @x: torch.Tensor((B,3,64,64))
+        @return: torch.Tensor((B,6))
+        Hint: Apply input normalization inside the network, to make sure it is applied in the grader
+        """
+        x = self.feature_extractor(x).mean(3).mean(2)
+        return self.classifier(x)
 
 
 class Detector(torch.nn.Module):
+    class UpBlock(torch.nn.Module):
+        def __init__(self, n_input, n_output, kernel_size=3, stride=2):
+            super().__init__()
+            self.c1 = torch.nn.ConvTranspose2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
+                                      stride=stride, output_padding=1)
+
+        def forward(self, x):
+            return F.relu(self.c1(x))
+    
     def __init__(self):
         """
            Your code here.
            Setup your detection network
         """
         super().__init__()
-        raise NotImplementedError('Detector.__init__')
+        self.input_mean = torch.Tensor([0.3521554, 0.30068502, 0.28527516])
+        self.input_std = torch.Tensor([0.18182722, 0.18656468, 0.15938024])
+        layers = [32, 64, 128]
+        c = 3
+        self.use_skip = True
+        self.n_conv = len(layers)
+        skip_layer_size = [3] + layers[:-1]
+        for i, l in enumerate(layers):
+            self.add_module('conv%d' % i, CNNClassifier.Block())
+            c = l
+        for i, l in list(enumerate(layers))[::-1]:
+            self.add_module('upconv%d' % i, self.UpBlock(c, l, 3, 2))
+            c = l
+            if self.use_skip:
+                c += skip_layer_size[i]
+        self.classifier = torch.nn.Conv2d(c, 3, 1)
 
     def forward(self, x):
         """
@@ -30,7 +122,21 @@ class Detector(torch.nn.Module):
            Implement a forward pass through the network, use forward for training,
            and detect for detection
         """
-        raise NotImplementedError('Detector.forward')
+        z = (x - self.input_mean[None, :, None, None].to(x.device)) / self.input_std[None, :, None, None].to(x.device)
+        up_activation = []
+        for i in range(self.n_conv):
+            # Add all the information required for skip connections
+            up_activation.append(z)
+            z = self._modules['conv%d' % i](z)
+
+        for i in reversed(range(self.n_conv)):
+            z = self._modules['upconv%d' % i](z)
+            # Fix the padding
+            z = z[:, :, :up_activation[i].size(2), :up_activation[i].size(3)]
+            # Add the skip connection
+            if self.use_skip:
+                z = torch.cat([z, up_activation[i]], dim=1)
+        return self.classifier(z)
 
     def detect(self, image):
         """
@@ -45,7 +151,7 @@ class Detector(torch.nn.Module):
                  scalar. Otherwise pytorch might keep a computation graph in the background and your program will run
                  out of memory.
         """
-        raise NotImplementedError('Detector.detect')
+        return [[(*peak, 0, 0) for peak in extract_peak(heatmap, max_pool_ks=11, max_det=15)] for heatmap in self(image[None]).squeeze(0)]
 
 
 def save_model(model):
--- homework4/homework/train.py
+++ homework4/homework/train.py
@@ -19,7 +19,46 @@ def train(args):
     Your code here, modify your HW3 code
     Hint: Use the log function below to debug and visualize your model
     """
-    raise NotImplementedError('train')
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model.to(device)
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, momentum=args.momentum, weight_decay=args.weight_decay)
+    loss = torch.nn.BCEWithLogitsLoss()
+    train_data = load_detection_data('dense_data/train')
+    valid_data = load_detection_data('dense_data/valid')
+    loss.to(device)
+    global_step = 0
+    for epoch in range(args.num_epoch):
+        print("epoch #" + str(epoch))
+        print("Training...")
+        model.train()
+        loss_vals, acc_vals, vacc_vals = [], [], []
+        for im, hm, _ in train_data:
+            im, hm = im.to(device), hm.to(device)
+            pred = model(im)
+            loss_val = loss(pred, hm).mean()
+            acc_val = (pred.argmax(1) == hm).float().mean().item()
+
+            loss_vals.append(loss_val.detach().cpu().numpy())
+            acc_vals.append(acc_val)
+            optimizer.zero_grad()
+            loss_val.backward()
+            optimizer.step()
+            global_step += 1
+            log(train_logger, im, hm, pred, global_step)
+
+        avg_loss = sum(loss_vals) / len(loss_vals)
+        avg_acc = sum(acc_vals) / len(acc_vals)
+        model.eval()
+        print("Validating...")
+        for im, hm, _ in valid_data:
+            im, hm = im.to(device), hm.to(device)
+            vacc_vals.append((model(im).argmax(1) == hm).float().mean().item())
+            log(train_logger, im, hm, pred, global_step)
+            
+
+        avg_vacc = sum(vacc_vals) / len(vacc_vals)
+        valid_logger.add_scalar('accuracy', avg_vacc, global_step)
+        print('epoch %-3d \t loss = %0.3f \t acc = %0.3f \t val acc = %0.3f \t iou = %0.3f' % (epoch, avg_loss, avg_acc, avg_vacc))
     save_model(model)
 
 
@@ -41,7 +80,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--log_dir')
-    # Put custom arguments here
+    parser.add_argument('-n', '--num_epoch', type=int, default=50)
+    parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3)
+    parser.add_argument('-mo', '--momentum', type=float, default=.9)
+    parser.add_argument('-wd', '--weight_decay', type=float, default=1e-4)  
 
     args = parser.parse_args()
     train(args)
