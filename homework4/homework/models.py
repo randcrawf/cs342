a24155cabc4c8f6b2a028eb053140af91c983ef7
Randall Crawford <rmc3637@utexas.edu>
2021-10-30 18:31:18 -0500

    commit

--- homework4/homework/models.py
+++ homework4/homework/models.py
@@ -47,7 +47,7 @@ def extract_peak(heatmap, max_pool_ks=7, min_score=-5, max_det=100):
     
     mask = torch.logical_and(local_maxs > min_score, is_peak == 1.0)
     #mask = []
-    print(mask.size())
+    print(torch.bool(1,1,mask.size(2), mask.size(3)).size())
     local_maxs = local_maxs[mask]
     indices = indices[mask]
     peaks, inds = torch.topk(local_maxs, min(max_det, len(local_maxs)))
