70e034d604efeaa18ee3f45bf5c28b2ad1cccfcd
Randall Crawford <rmc3637@utexas.edu>
2021-10-31 16:14:56 -0500

    commit

--- homework4/homework/train.py
+++ homework4/homework/train.py
@@ -23,7 +23,7 @@ def train(args):
     model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
     loss = torch.nn.BCEWithLogitsLoss()
-    train_data = load_detection_data('dense_data/train', transform=dense_transforms.Compose([dense_transforms.ToTensor(), dense_transforms.ToHeatmap()]))
+    train_data = load_detection_data('dense_data/train', transform=dense_transforms.Compose([dense_transforms.ColorJitter(0.9, 0.9, 0.9, 0.1), dense_transforms.RandomHorizontalFlip(), dense_transforms.ToTensor(), dense_transforms.ToHeatmap()]))
     valid_data = load_detection_data('dense_data/valid', transform=dense_transforms.Compose([dense_transforms.ToTensor(), dense_transforms.ToHeatmap()]))
     loss.to(device)
     global_step = 0
