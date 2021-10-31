e3f724522779185ef0dcb77bbbcfad3c04b5835d
Randall Crawford <rmc3637@utexas.edu>
2021-10-31 17:48:26 -0500

    commit

--- homework4/homework/train.py
+++ homework4/homework/train.py
@@ -73,7 +73,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--log_dir')
-    parser.add_argument('-n', '--num_epoch', type=int, default=20)
+    parser.add_argument('-n', '--num_epoch', type=int, default=25)
     parser.add_argument('-lr', '--learning_rate', type=float, default=5e-3)
     parser.add_argument('-mo', '--momentum', type=float, default=.9)
     parser.add_argument('-wd', '--weight_decay', type=float, default=1e-6)  
